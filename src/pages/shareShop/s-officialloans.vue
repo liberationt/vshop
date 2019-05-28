d32d07a1f9d9ad92d04da528c4d3506914204a68
tianjianmin <1254845399@qq.com>
2019-05-28 18:36:13 +0800

    代码合并

--- src/pages/shareShop/s-officialloans.vue
+++ src/pages/shareShop/s-officialloans.vue
@@ -1,27 +1,56 @@
 <template>
-    <div>
-        <h1>官方贷款</h1>
-				<ul>
-					<li v-for="(item,i) in officialdataList" :key="i">
-						<div><img src="" alt=""></div>
-						<div>
-								<h4>标题</h4>
-								<p>打点爱的色放阿斯顿</p>
-								<p>大大是阿斯顿是</p>
-						</div>
-					</li>
-				</ul>
-    </div>
+	<div>
+		<div class='tochiose'>
+			<div class="netloan" @click="tonetloan">网贷</div>
+			<div class="amount " @click="toamount">大额</div>
+		</div>
+		<div class="tittle">
+			<p>申请多个产品，可大幅提高贷款成功率</p>
+			<div><img src="./images/shuaxin.png" alt="">换一批</div>
+		</div>
+		<div class="listdata">
+			<div class="listdatatop">
+				<div>
+					<div><img src="" alt=""></div>
+				</div>
+				<div>
+					<h4>大王贷款</h4>
+					<p>“门槛低 额度大 放款快 费率低”</p>
+				</div>
+			</div>
+			<div class="listdatabot">
+				<div>
+					<p style="font-size:16px;color:#FE951E">1万-20万</p>
+					<p>可用额度 (元)</p>
+				</div>
+				<div>
+					<p>期限：<span>12个月-36个月</span></p>
+					<p>最快当天到账</p>
+				</div>
+				<div>立即申请</div>
+			</div>
+		</div>
+	</div>
 </template>
 <script>
 export default {
     data(){
         return{
-					officialdataList:[1,2,3,4,5,6,7,8,9,10]
+					officialdataList:[1]
         }
     },
     methods:{
+			todolist(){
+				alert(1)
+			},
+			//网贷
+			tonetloan(){
 
+			},
+			//大额
+			toamount(){
+
+			}
     },
     mounted(){
 
@@ -29,5 +58,81 @@ export default {
 }
 </script>
 <style lang="less">
-    
+	.tochiose{
+		display: flex;
+		justify-content: space-between;
+		margin-bottom: 14px;
+		div{
+			width:166px;height:51px;
+			font-size:20px;
+			line-height: 51px;
+			color:#ffffff;
+			text-indent: 87px;
+			font-weight:bold;
+		}
+		.netloan{
+			background:url(./images/netloan.png);
+			background-size:100%;
+		}
+		.amount{
+			background:url(./images/amount.png);
+			background-size:100%;
+		}
+	} 
+	.tittle{
+		height:40px;
+		background: #ffffff;
+		display: flex;
+		justify-content: space-between;
+		align-items: center;
+		padding:0 15px;
+		font-size:12px;
+		color:#4897FF;
+		margin-bottom: 5px;
+		img{
+			width:16px;
+			height:14px;
+			margin-right: 8px;
+		}
+	}
+	.listdata{
+		margin-bottom:10px;
+		background: #ffffff;
+		.listdatatop{
+			height:53px;
+			border-bottom:1px solid #E7E7E7 /*no*/;
+			padding:0 15px;
+			display: flex;
+			align-items: center;
+			img{
+				width:33px;
+				height:33px;
+				margin-right: 10px;
+			}
+			h4{
+				font-size:16px;
+				color:#333333;
+				font-weight: bold;
+			}
+			p{
+				font-size: 11px;
+				color:#999999;
+				margin-top: 3px;
+			}
+		}
+		.listdatabot{
+			display: flex;
+			padding:0 15px;
+			justify-content: space-between;
+			align-items: center;
+			height:58px;
+			font-size:12px;
+			color:#333333;
+			div{
+				display: flex;
+				flex-direction: column;
+				justify-content: space-between;
+			}
+		}
+	}
 </style>
--- src/pages/shareShop/s-shoppage.vue
+++ src/pages/shareShop/s-shoppage.vue
@@ -1,37 +1,318 @@
 <template>
-    <div>
-        <section>
-            <ul>
-                <li v-for="(item,i) in menuList" :key="i"></li>
-            </ul>
-        </section>
-        <footer>
-            <ul>
-                <li v-for="(item,v) in tweetsList" :key="v"></li>
-            </ul>
-        </footer>
-    </div>
+	<div class="shoppagemain">
+		<div class="personalinfor">
+			<header>
+				<div class="headerlift">
+					<div class="personal"> <img src="" alt=""></div>
+					<p>邀请码6S89WH</p>
+				</div>
+				<div class="headerright">
+					<h4>店铺名称您的信贷专家</h4>
+					<div>
+						<span>成都</span>
+						<span>郑经理</span>
+						<span class="identity">身份认证</span>
+					</div>
+					<span class="headerbot">这里是店铺介绍欢迎来电咨询</span>
+				</div>
+			</header>
+			<div class="contact">
+				<div class="contactleft"><img src="./images/dianhua.png" alt="">电话联系</div>
+				<div class="contactright" @click="weixin"><img src="./images/weixin.png" alt="">微信联系</div>
+			</div>
+			<div class="entrance">
+				<div @click="loan">
+					<div class="daikuan"><img src="./images/daikuan.png" alt=""></div>
+					<div>
+						<h4>我要贷款</h4>
+						<p>利率低 额度高 下款快</p>
+						<p>多种产品任你选</p>
+					</div>
+				</div>
+				<div @click="card">
+					<div class="banka"><img src="./images/banka.png" alt=""></div>
+					<div>
+						<h4>我要办卡</h4>
+						<p>各种热门信用卡</p>
+						<p>额度高批卡率99%</p>
+					</div>
+				</div>
+				<div @click="tool">
+					<div class="gongju"><img src="./images/gongju.png" alt=""></div>
+					<div>
+						<h4>实用小工具</h4>
+						<p>查征信 查网贷黑名单</p>
+						<p>在线实时查询%</p>
+					</div>
+				</div>
+			</div>
+		</div>
+		<div class="tweetsList">
+			<h4><span></span>推荐阅读</h4>
+			<div v-for="(item,i) in dataList" :key="i" @click="todetails">
+				<div><img src="./images/people.png" alt=""></div>
+				<div class="listright">
+					<p>信贷经理教你如何高效贷款</p>
+					<span>2019-05-10</span>
+				</div>
+			</div>
+		</div>
+		<div class="dialog">
+			<van-dialog
+				v-model="show"
+				show-cancel-button
+				cancel-button-text='关闭'
+				confirm-button-text='复制微信ID'
+				confirmButtonColor=""
+				confirmButtonBackground='red'
+				:beforeClose="beforeClose"
+			>
+			<div class="dialogbanner"></div>
+			<div class="dialogtitle">
+				<p>长按识别二维码图片</p>
+				<p>或复制微信ID搜索添加</p>
+			</div>
+			</van-dialog>
+		</div>
+	</div>
 </template>
 <script>
 export default {
-    data(){
-        return{
-            menuList:[],
-            tweetsList:[]
-        }
-    },
-    methods:{
-        getDatas(){
-            this.request('',{}).then((data)=>{
-                this.data = data.data
-            })
-        }
-    },
-    mounted(){
-        this.$emit('toparent','店铺首页',1)
-    }
+	data(){
+		return{
+			show:true,
+			dataList:[1]
+		}
+	},
+	methods:{
+		getDatas(){
+			this.request('',{}).then((data)=>{
+					this.data = data.data
+			})
+		},
+		loan(){
+			this.$router.push('')
+		},
+		card(){
+			this.$router.push('')
+		},
+		tool(){
+			this.$router.push('')
+		},
+		todetails(){
+			this.$router.push('')
+		},
+		weixin(){
+			this.show=true
+		},
+		beforeClose(action,down){
+				if(action==='confirm'){
+					this.confirm()
+					down()
+				}else{
+					this.show= false
+					down()
+				}
+		},
+		confirm(){
+			alert(1)
+			this.show= false
+		
+		}
+	},
+	mounted(){
+			this.$emit('toparent','店铺首页',1)
+	}
 }
 </script>
-<style lang="less">
-    
+<style lang="less" scoped>
+   .shoppagemain{
+		background: #f1f1fb;
+		height:100%;
+		.personalinfor{
+			padding:15px;
+			header{
+				display: flex;
+			}
+			.headerlift{
+				margin-right: 11px;
+				text-align:center;
+				.personal{
+					width:80px;
+					height:80px;
+					background: #000;
+					border-radius: 50%;
+					margin:0 auto;
+					img{
+						width:100%;
+						height:100%;
+					}
+				}
+				p{
+					width:85px;
+					height:19px;
+					background:rgba(254,241,227,1);
+					border:1px solid rgba(243,177,62,1) /*no*/;
+					border-radius:9px;
+					font-size:10px;
+					color:#F3B13E;
+					line-height: 19px;
+					margin-top: -10px;
+				}
+			}
+			.headerright{
+				display: flex;
+				flex-direction: column;
+				justify-content: space-around;
+				color:#333333;
+				h4{
+					font-size:14px;
+					font-weight:bold;
+				}
+				div{
+					font-size:16px;
+					display: flex;
+					align-items: center;
+					>span{
+						margin-right: 10px;
+					}
+					.identity{
+						display: block;
+						background: #F3B13E;
+						font-size:10px;
+						color:#ffffff;
+						padding:0 8px;
+						height:17px;
+						line-height: 17px !important;
+						border-radius: 3px;
+						// margin-top: 2px;
+						// line-height: 17px;
+					}
+				}
+				.headerbot{
+					color:#666666;
+					font-size:14px;
+				}
+			}
+			.contact{
+				margin: 20px 0 15px;
+				display: flex;
+				justify-content: space-between;
+				font-size:16px;
+				color:#ffffff;
+				.contactleft{
+					width:139px;
+					height:45px;
+					background: #4597FB;
+					text-align: center;
+					line-height: 45px;
+					border-radius: 10px;
+					img{
+						width:25px;
+						height:25px;
+						margin-right: 10px;
+					}
+				}
+				.contactright{
+					width:150px;
+					height:45px;
+					background: #62D62D;
+					text-align: center;
+					line-height: 45px;
+					border-radius: 10px;
+					img{
+						width:32px;
+						height:26px;
+						margin-right: 10px;
+					}
+				}
+			}
+			.entrance{
+				>div{
+					height:105px;
+					background: #ffffff;
+					margin-bottom:10px;
+					display: flex;
+					align-items: center;
+					padding-left:34px;
+					color:#333333;
+					h4{
+						font-size:16px;
+						font-weight: bold;
+					}
+					p{
+						font-size:15px;
+					}
+					.daikuan{
+						width:57px;height:75px;
+						margin-right: 22px;
+					}
+					.banka{
+						width:66px;
+						height:65px;
+						margin-right: 15px;
+					}
+					.gongju{
+						width:64px;
+						height:80px;
+						margin-right: 19px;
+					}
+				}
+			}
+		}
+		.tweetsList{
+			background: #ffffff;
+			padding-left:15px;
+			>div{
+				height:95px;
+				display: flex;
+				align-items: center;
+				font-size:12px;
+				color:#666666;
+				img{
+					width:106px;
+					height:65px;
+					margin-right: 8px;
+				}
+				p{
+					margin-bottom:18px;
+					font-size:15px;
+					color:#333333;
+				}
+			}
+			h4{
+				display: flex;
+				align-items: center;
+				height:44px;
+				line-height:44px;
+				font-size:17px;
+				color:#4597FB;
+				span{
+					display: inline-block;
+					height:18px;
+					width:3px;
+					border-radius: 2px;
+					background: #4597FB;
+					margin-right: 8px;
+					margin-top: -2px;
+				}
+			}
+		}
+		.dialog{
+			.dialogbanner{
+				height:166px;
+				background: red;
+			}
+			p{
+				text-align: center;
+
+			}
+		}
+		.dialogtitle{
+			margin:15px auto 20px;
+			font-size:14px;
+		}
+
+		
+	}
 </style>
