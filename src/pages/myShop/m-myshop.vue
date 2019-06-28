89f5b7345fa1eb8e09fb510c9233526612346e94
liulili <liulili@netwisdom.cn>
2019-06-28 14:47:22 +0800

    分享

--- src/pages/myShop/m-myshop.vue
+++ src/pages/myShop/m-myshop.vue
@@ -137,14 +137,14 @@ export default {
     wxShare(inviterCode) {
       console.log(window.location)
       this.request("wisdom.vshop.wechatOpen.getJsconf", {
-        url: window.location.origin + "/shoppage?storeCode="+inviterCode
+        url: window.location.origin + "/shoppage?inviterCode="+inviterCode
       }).then(data => {
           utils.wxShare(data.data)
           wx.ready(function(){
             wx.updateAppMessageShareData({
               title: '急用钱？请找我，专业贷款！', // 分享标题
               desc: '*经理向您推荐了自己的微店，提供工资贷、社保贷、消费贷、公积金贷、车贷房贷……规渠正规安全，服务专业周到，快来看看吧！', // 分享描述
-              link: window.location.origin + "/shoppage?storeCode="+inviterCode, // 分享链接，该链接域名或路径必须与当前页面对应的公众号JS安全域名一致
+              link: window.location.origin + "/shoppage?inviterCode="+inviterCode, // 分享链接，该链接域名或路径必须与当前页面对应的公众号JS安全域名一致
               imgUrl: 'http://thirdwx.qlogo.cn/mmopen/vi_32/ZOfgNPVEofm6wGcqrYFDwxAhllW0k3wUom1HXIlmoeQYPf8YX0FkagGibAvcE9dlyLXIRlbicpjacA9wDDR6yU8g/132', // 分享图标
               success: function () {
                 // 用户点击了分享后执行的回调函数
