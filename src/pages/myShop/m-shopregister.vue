965e175386534e53194049b33871c4daf7a1d950
liulili <liulili@netwisdom.cn>
2019-07-01 16:08:48 +0800

    手机号脱敏

--- src/pages/myShop/m-shopregister.vue
+++ src/pages/myShop/m-shopregister.vue
@@ -41,7 +41,7 @@
                     <p class="one">{{item.dataCreateTime}}</p>
                     <p class="two">
                       <span>{{item.userName}}</span>
-                      <span>{{item.userPhone}}</span>
+                      <span>{{item.textPhone}}</span>
                       <span class="message_icon" @click.stop="goMessage(item.userPhone)"><img src="./imgs/message_icon@2x.png" alt=""></span>
                       <span class="phone_icon" @click.stop="goPhone(item.userPhone)"><img src="./imgs/phone_icon@2x.png" alt=""></span>
                       <span class="biajidianpu"><img src="./imgs/biajidianpu.png" alt=""></span>
@@ -241,27 +241,7 @@ export default {
       })
         .then(() => {
           if(this.generalizeStore.hasStore){
-              const copyToClipboard = str => {
-              const el = document.createElement('textarea'); // Create a <textarea> element
-              el.value = str; // Set its value to the string that you want copied
-              el.setAttribute('readonly', ''); // Make it readonly to be tamper-proof
-              el.style.position = 'absolute'; 
-              el.style.left = '-9999px'; // Move outside the screen to make it invisible
-              document.body.appendChild(el); // Append the <textarea> element to the HTML document
-              const selected = 
-              document.getSelection().rangeCount > 0 // Check if there is any content selected previously
-              ? document.getSelection().getRangeAt(0) // Store selection if found
-              : false; // Mark as false to know no selection existed before
-              el.select(); // Select the <textarea> content
-              document.execCommand('copy'); // Copy - only works as a result of a user action (e.g. click events)
-              document.body.removeChild(el); // Remove the <textarea> element
-              if (selected) { // If a selection existed before copying
-              document.getSelection().removeAllRanges(); // Unselect everything on the HTML document
-              document.getSelection().addRange(selected); // Restore the original selection
-              }
-             };
-             copyToClipboard(this.generalizeStore.text + this.generalizeStore.shortLink)
-            // utils.copyContent(this.generalizeStore.text + this.generalizeStore.shortLink)
+            utils.copyContent(this.generalizeStore.text + this.generalizeStore.shortLink)
             Toast('复制成功')
           } else {
             this.$router.push({path:'./meditshop'})
