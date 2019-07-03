e796dadd24303669f9bbf1d247ccf010d624037b
liulili <liulili@netwisdom.cn>
2019-07-03 10:35:36 +0800

    分享

--- src/pages/myShop/m-myshop.vue
+++ src/pages/myShop/m-myshop.vue
@@ -156,26 +156,27 @@ export default {
         })
     },
     wxShare(inviterCode) {
-      console.log(window.location.href)
       this.request("wisdom.vshop.wechatOpen.getJsconf", {
-        url: window.location.href
+        url: window.location.origin+'/myshop'
       }).then(data => {
           utils.wxShare(data.data)
-          wx.ready(function(){
-            wx.updateAppMessageShareData({
-              title: '急用钱？请找我，专业贷款！', // 分享标题
-              desc: '*经理向您推荐了自己的微店，提供工资贷、社保贷、消费贷、公积金贷、车贷房贷……规渠正规安全，服务专业周到，快来看看吧！', // 分享描述
-              link: window.location.origin + "/shoppage?inviterCode="+inviterCode, // 分享链接，该链接域名或路径必须与当前页面对应的公众号JS安全域名一致
-              imgUrl: 'http://thirdwx.qlogo.cn/mmopen/vi_32/ZOfgNPVEofm6wGcqrYFDwxAhllW0k3wUom1HXIlmoeQYPf8YX0FkagGibAvcE9dlyLXIRlbicpjacA9wDDR6yU8g/132', // 分享图标
-              success: function () {
-                // 用户点击了分享后执行的回调函数
-                alert('分享成功回调')
-              },
-              cancel: function(err){
-                alert('分享取消回调')
-              }
-            });
-          })
+          setTimeout(() => {
+            wx.ready(function(){
+              wx.updateAppMessageShareData({
+                title: '急用钱？请找我，专业贷款！', // 分享标题
+                desc: '*经理向您推荐了自己的微店，提供工资贷、社保贷、消费贷、公积金贷、车贷房贷……规渠正规安全，服务专业周到，快来看看吧！', // 分享描述
+                link: window.location.origin + "/shoppage?inviterCode="+inviterCode, // 分享链接，该链接域名或路径必须与当前页面对应的公众号JS安全域名一致
+                imgUrl: 'http://thirdwx.qlogo.cn/mmopen/vi_32/ZOfgNPVEofm6wGcqrYFDwxAhllW0k3wUom1HXIlmoeQYPf8YX0FkagGibAvcE9dlyLXIRlbicpjacA9wDDR6yU8g/132', // 分享图标
+                success: function () {
+                  // 用户点击了分享后执行的回调函数
+                  alert('分享成功回调')
+                },
+                cancel: function(err){
+                  alert('分享取消回调')
+                }
+              });
+            })
+　　　　   }, 500)
         })
         .catch(err => {
           console.log(err);
@@ -191,7 +192,7 @@ export default {
       .catch(err => {
         console.log(err);
       });
-    
+    // 获取邀请码
     this.request("wisdom.vshop.vshopStoreManager.getShareRes", {})
     .then(data => {
       this.inviterCode = data.data.inviterCode
