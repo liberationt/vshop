4c2e904c884b3620cd91c610473980bd5bf82a19
liulili <liulili@netwisdom.cn>
2019-07-03 17:44:53 +0800

    一键代理

--- src/pages/myShop/cards/instruments.vue
+++ src/pages/myShop/cards/instruments.vue
@@ -48,7 +48,7 @@
         </p>
       </div>
     </van-popup>
-    <footer v-if="showStatus == 1" class="footer_button" @click="moneyShow = true">
+    <footer v-if="showStatus == 1" class="footer_button" @click="moneyShow = true,agentStatus=1">
       <button>一键代理推广赚工资</button>
     </footer>
     <!-- 弹出层 -->
