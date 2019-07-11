9f3301ca5fc45c6dde93d74ed2d8d7c3d7b0d672
liulili <liulili@netwisdom.cn>
2019-07-11 11:24:22 +0800

    分享图片

--- src/pages/myShop/m-selfshopdetails.vue
+++ src/pages/myShop/m-selfshopdetails.vue
@@ -124,7 +124,7 @@ export default {
               title: dataList.title, // 分享标题
               desc: dataList.shareContent, // 分享描述
               link: dataList.url, // 分享链接，该链接域名或路径必须与当前页面对应的公众号JS安全域名一致
-              imgUrl: 'http://thirdwx.qlogo.cn/mmopen/vi_32/ZOfgNPVEofm6wGcqrYFDwxAhllW0k3wUom1HXIlmoeQYPf8YX0FkagGibAvcE9dlyLXIRlbicpjacA9wDDR6yU8g/132', // 分享图标
+              imgUrl: 'https://wisdom-loan.oss-cn-shanghai.aliyuncs.com/productParam/60938f68-1fa0-4620-a90a-7a4d7a7c7117.png', // 分享图标
               success: function () {
                 // 用户点击了分享后执行的回调函数
                 alert('分享成功回调')
